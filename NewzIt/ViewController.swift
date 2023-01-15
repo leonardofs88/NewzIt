7065f60be6362c8627cc7f1fa97ef28c71339cbb
Leonardo Soares <leonardofs88@live.com>
2023-01-15 12:11:43 +0000

    Initial Commit

---
+++ NewzIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewzIt
+//
+//  Created by Leonardo Soares on 15/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
